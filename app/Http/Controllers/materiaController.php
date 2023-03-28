cf31986e1930cfcbafd519d5ce66465a9da7b9b0
17012562 <danielasusanox14@gmail.com>
2023-03-28 12:55:19 -0600

    Se agregaron los Logs en MateriaController

--- app/Http/Controllers/materiaController.php
+++ app/Http/Controllers/materiaController.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Controllers;
 use App\Models\m_materia;
-
-
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class materiaController extends Controller
 {
@@ -12,36 +12,65 @@ class materiaController extends Controller
         return view('materia');
     }
     public function lista(){
-        $materia = m_materia::all();
-        return $materia;
+        #Primer try
+        try{
+            $materia = m_materia::all();
+            return $materia;
+        }catch(Exception $e){
+            Log::error('Metodo Lista clase MateriaController->' .$e->getMessage());
+        }
+        
     }
     public function materia(Request $request){
-        $materia = m_materia::find($request->id);
-        return $materia;
+        #Segundo try
+        try{
+            $materia = m_materia::find($request->id);
+            return $materia;
+        }catch(Exception $e){
+            Log::error('Metodo materia clase MateriaController->' .$e->getMessage());
+        }
+        
     }
     public function guardar(Request $request){
-        if($request->id == 0){
-            $materia
-             = new m_materia();
+        #Tercer try
+        try{
+            if($request->id == 0){
+                $materia
+                 = new m_materia();
+            }
+            else {
+                $materia = m_materia::find($request->id);
+            }
+            $materia->nombre = $request->nombre;
+            $materia->profesor = $request->profesor;
+            $materia->horario = $request->horario;
+            $materia->save();
+    
+            return $materia;
+        }catch(Exception $e){
+            Log::error('Metodo Guardar clase MateriaController->' .$e->getMessage());
         }
-        else {
-            $materia = m_materia::find($request->id);
-        }
-        $materia->nombre = $request->nombre;
-        $materia->profesor = $request->profesor;
-        $materia->horario = $request->horario;
-        $materia->save();
-
-        return $materia;
+        
     }
     public function borrar(Request $request){
+        #Cuarto try
+        try{
+            $materia = m_materia::find($request->id);
+            $materia ->delete();
+            return "OK";
+        }catch(Exception $e){
+            Log::error('Metodo Borrar clase MateriaController->' .$e->getMessage());
 
-        $materia = m_materia::find($request->id);
-        $materia ->delete();
-        return "OK";
+        }
     }
     public function combo(){
-        $materia = m_materia::select('nombre as name', 'id as code')->get();
-        return $materia;
+        #Quinto try
+        try{
+            $materia = m_materia::select('nombre as name', 'id as code')->get();
+            return $materia;
+        }catch(Exception $e){
+            Log::error('Metodo Combo clase MateriaController->' .$e->getMessage());
+        }
+        
     }
 }
