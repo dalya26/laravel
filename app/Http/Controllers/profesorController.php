761ef7e2e0823d13c5fe719a7b0ff24e52e7579d
17012562 <danielasusanox14@gmail.com>
2023-03-28 13:07:17 -0600

    Logs en ProfesorController

--- app/Http/Controllers/profesorController.php
+++ app/Http/Controllers/profesorController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\m_profesor;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+
 
 class profesorController extends Controller
 {
@@ -11,47 +14,67 @@ class profesorController extends Controller
         return view('profesor');
     }
     public function lista(){
-        //$profesor = m_profesor::all();
-        $profesor = m_profesor::join('materia','profesor.id_materia','=','materia.id')
-        -> select('profesor.*', 'materia.nombre as nombre_materia')
-        ->get(); 
-        return $profesor;
+        #Primer try
+        try{
+            //$profesor = m_profesor::all();
+            $profesor = m_profesor::join('materia','profesor.id_materia','=','materia.id')
+            -> select('profesor.*', 'materia.nombre as nombre_materia')
+            ->get(); 
+            return $profesor;
+        }catch(Exception $e){
+            Log::error('Metodo Lista clase ProfesorController->' .$e->getMessage());
+        }
+        
     }
     public function profesor(Request $request){
-        $profesor = m_profesor::find($request->id);
-        return $profesor;
+        #Primer try
+        try{
+            $profesor = m_profesor::find($request->id);
+            return $profesor;
+        }catch(Exception $e){
+            Log::error('Metodo Profesor clase ProfesorController->' .$e->getMessage());
+        }
+        
     }
 
     public function guardar(Request $request){
-        if($request->id == 0){
-            $profesor = new m_profesor();
+        #Primer try
+        try{
+            if($request->id == 0){
+                $profesor = new m_profesor();
+            }
+            else {
+                $profesor = m_profesor::find($request->id);
+            }
+    
+            $profesor->nombre = $request->nombre;
+            $profesor->apetpat = $request->apetpat;
+            $profesor->apetmat = $request->apetmat;
+            $profesor->matricula = $request->matricula;
+            $profesor->edad = $request->edad;
+            $profesor->sexo = $request->sexo;
+            $profesor->cedula = $request->cedula;
+            $profesor->asignatura = $request->asignatura;
+            $profesor->habilidades = $request->habilidades;
+    
+            $profesor->id_materia = $request->id_materia;
+    
+            $profesor->save();
+    
+            return $profesor;
+        }catch(Exception $e){
+            Log::error('Metodo Guardar clase ProfesorController->' .$e->getMessage());
         }
-        else {
-            $profesor = m_profesor::find($request->id);
-        }
-
-        $profesor->nombre = $request->nombre;
-        $profesor->apetpat = $request->apetpat;
-        $profesor->apetmat = $request->apetmat;
-        $profesor->matricula = $request->matricula;
-        $profesor->edad = $request->edad;
-        $profesor->sexo = $request->sexo;
-        $profesor->cedula = $request->cedula;
-        $profesor->asignatura = $request->asignatura;
-        $profesor->habilidades = $request->habilidades;
-
-        $profesor->id_materia = $request->id_materia;
-
-        $profesor->save();
-
-        return $profesor;
     }
     public function borrar(Request $request){
-
-        $profesor = m_profesor::find($request->id);
-        $profesor ->delete();
-        return "OK";
+        #Primer try
+        try{
+            $profesor = m_profesor::find($request->id);
+            $profesor ->delete();
+            return "OK";
+        }catch(Exception $e){
+            Log::error('Metodo Borarr clase ProfesorController->' .$e->getMessage());
+        }
     }
     
-
 }
